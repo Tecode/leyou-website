9c70016379b55b4a41558fe631cdd654338ada33
Tecode <283731869@qq.com>
2017-07-08 16:58:25 +0800

    '完成图片上传'

--- src/components/articleEdit/ArticleEditItem.vue
+++ src/components/articleEdit/ArticleEditItem.vue
@@ -64,6 +64,50 @@
 			const E = window.wangEditor;
 			const editor = new E('#Edit');
 			editor.customConfig.uploadImgShowBase64 = true;
+			editor.customConfig.zIndex = 100;
+			editor.customConfig.uploadImgHooks = {
+				before: function (xhr, editor, files) {
+					// 图片上传之前触发
+					// xhr 是 XMLHttpRequst 对象，editor 是编辑器对象，files 是选择的图片文件
+				},
+				success: function (xhr, editor, result) {
+					// 图片上传并返回结果，图片插入成功之后触发
+					// xhr 是 XMLHttpRequst 对象，editor 是编辑器对象，result 是服务器端返回的结果
+				},
+				fail: function (xhr, editor, result) {
+					// 图片上传并返回结果，但图片插入错误时触发
+					// xhr 是 XMLHttpRequst 对象，editor 是编辑器对象，result 是服务器端返回的结果
+				},
+				error: function (xhr, editor) {
+					// 图片上传出错时触发
+					// xhr 是 XMLHttpRequst 对象，editor 是编辑器对象
+				},
+				timeout: function (xhr, editor) {
+					// 图片上传超时时触发
+					// xhr 是 XMLHttpRequst 对象，editor 是编辑器对象
+				},
+
+				// 如果服务器端返回的不是 {errno:0, data: [...]} 这种格式，可使用该配置
+				// （但是，服务器端返回的必须是一个 JSON 格式字符串！！！否则会报错）
+				customInsert: function (insertImg, result, editor) {
+					console.log(result);
+					// 图片上传并返回结果，自定义插入图片的事件（而不是编辑器自动插入图片！！！）
+					// insertImg 是插入图片的函数，editor 是编辑器对象，result 是服务器端返回的结果
+
+					// 举例：假如上传图片成功后，服务器端返回的是 {url:'....'} 这种格式，即可这样插入图片：
+                    if (result) {
+                    	result.data.map( (url) => {
+						    insertImg(url)
+                        })
+                    }
+
+					// result 必须是一个 JSON 格式字符串！！！否则报错
+				}
+			};
+			// 将图片大小限制为 3M
+			editor.customConfig.uploadImgMaxSize = 3 * 1024 * 1024;
+			editor.customConfig.uploadImgServer = '/upload';
+
 			editor.create()
 		},
 		computed: {},
@@ -136,6 +180,7 @@
             .title {
                 font-size: 16px;
                 color: @manager_color15;
+                margin-bottom: 15px;
             }
             label {
                 position: relative;
@@ -177,6 +222,9 @@
                 color: @text100;
                 margin: 0 25px 0 0;
             }
+        }
+        #Edit {
+            min-height: 800px;
         }
     }
 </style>
